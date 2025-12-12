b32ef89cb133fe8e506cd7dc0b5ce6db11bf3b31
CancerM <cancermqiao@gmail.com>
2025-12-12 17:53:03 +0800

    update

--- build-nanogpt/train_gpt2.py
+++ build-nanogpt/train_gpt2.py
@@ -6,10 +6,11 @@ from torch.nn import functional as F
 
 
 class CausalSelfAttention(nn.Module):
+
     def __init__(self, config):
         super().__init__()
         assert config.n_embd % config.n_head == 0
-        # q, k, v in all 
+        # q, k, v in all
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         # output projection
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
@@ -17,46 +18,50 @@ class CausalSelfAttention(nn.Module):
         self.n_head = config.n_head
         self.n_embd = config.n_embd
         # mask matrics, following the OpenAI/HF naming
-        self.register_buffer('bias', torch.tril(torch.ones(config.block_size, config.block_size)).view(1, 1, config.block_size, config.block_size))
-    
+        self.register_buffer(
+            'bias',
+            torch.tril(torch.ones(config.block_size, config.block_size)).view(
+                1, 1, config.block_size, config.block_size))
+
     def forward(self, x):
-        B, T, C = x.size() # (batch_size, sequence_length, embedding_size)
+        B, T, C = x.size()  # (batch_size, sequence_length, embedding_size)
         # multi head q, k, v in all
         # C = n_embedding = n_head * hs
         # e.g. in GPT-2 (124M), n_head=12, hs=64, C=n_embedding=768
         qkv = self.c_attn(x)
         q, k, v = qkv.split(self.n_embd, dim=2)
-        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
         k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
-        
+
         att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
         # mask
-        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
         att = F.softmax(att, dim=-1)
-        y = att @ v # (B, nh, T, T) X (B, nh, T, hs) -> (B, nh, T, hs)
-        y = y.transpose(1, 2).contiguous().view(B, T, C) # (B, T, C)
+        y = att @ v  # (B, nh, T, T) X (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C)  # (B, T, C)
         # output projection
-        y = self.c_proj(y) # (B, T, C)
+        y = self.c_proj(y)  # (B, T, C)
         return y
 
 
 class MLP(nn.Module):
+
     def __init__(self, config):
         super().__init__()
         self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
         self.gelu = nn.GELU(approximate='tanh')
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
-    
+
     def forward(self, x):
         x = self.c_fc(x)
         x = self.gelu(x)
         x = self.c_proj(x)
-        torch.tril()
         return x
 
 
 class Block(nn.Module):
+
     def __init__(self, config):
         super().__init__()
         self.ln_1 = nn.LayerNorm(config.n_embd)
@@ -73,25 +78,64 @@ class Block(nn.Module):
 @dataclass
 class GPTConfig:
     block_size: int = 1024
-    vocab_size: int = 50257 # 50,000 BPE merges + 256 bytes tokens + 1  <|endoftext|>
+    vocab_size: int = 50257  # 50,000 BPE merges + 256 bytes tokens + 1  <|endoftext|>
     n_layer: int = 12
     n_head: int = 12
     n_embd: int = 768
 
 
 class GPT(nn.Module):
+
     def __init__(self, config):
         super().__init__()
         self.config = config
 
-        self.transformer = nn.ModuleDict(dict(
-            wte = nn.Embedding(config.vocab_size, config.n_embd),
-            wpe = nn.Embedding(config.block_size, config.n_embd),
-            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
-            ln_f = nn.LayerNorm(config.n_embd)
-        ))
+        self.transformer = nn.ModuleDict(
+            dict(wte=nn.Embedding(config.vocab_size, config.n_embd),
+                 wpe=nn.Embedding(config.block_size, config.n_embd),
+                 h=nn.ModuleList(
+                     [Block(config) for _ in range(config.n_layer)]),
+                 ln_f=nn.LayerNorm(config.n_embd)))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+        
+        # weight sharing scheme
+        self.transformer.wte.weight = self.lm_head.weight
+        
+        # init params
+        self.apply(self._init_weights)
+        # apply special scaled init to the residual projections, per GPT-2 paper
+        for pn, p in self.named_parameters():
+            if pn.endswith('c_proj.weight'):
+                torch.nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * config.n_layer))
 
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+        
+    def forward(self, idx, targets=None):
+        # idx is of shape (B, T)
+        B, T = idx.size()
+        assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
+        # forward the token and position embeddings
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device)
+        pos_emb = self.transformer.wpe(pos) # position embeddings of shape (T, n_embd)
+        tok_emb = self.transformer.wte(idx) # token embeddings of shape (B, T, n_embd)
+        x = tok_emb + pos_emb # (B, T, n_embd)
+        # forward the blocks of the transformer
+        for block in self.transformer.h:
+            x = block(x)
+        # forward the final layernorm and the classifier
+        x = self.transformer.ln_f(x) # (B, T, n_embd)
+        logits = self.lm_head(x) # (B, T, vocab_size)
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+        return logits, loss
+        
     @classmethod
     def from_pretrained(cls, model_type):
         """Loads pretained GPT-2 model weights from huggingface"""
@@ -100,10 +144,13 @@ class GPT(nn.Module):
         print(f"loading weights from pretained gpt: {model_type}")
 
         config_args = {
-            'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
-            'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
-            'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
-            'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
+            'gpt2': dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
+            'gpt2-medium': dict(n_layer=24, n_head=16,
+                                n_embd=1024),  # 350M params
+            'gpt2-large': dict(n_layer=36, n_head=20,
+                               n_embd=1280),  # 774M params
+            'gpt2-xl': dict(n_layer=48, n_head=25,
+                            n_embd=1600),  # 1558M params
         }[model_type]
         config_args['vocab_size'] = 50257
         config_args['block_size'] = 1024
@@ -114,7 +161,7 @@ class GPT(nn.Module):
         sd = model.state_dict()
         sd_keys = sd.keys()
         sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')]
-        
+
         # init a huggingface/transformers model
         model_hf = GPT2LMHeadModel.from_pretrained(model_type)
         sd_hf = model_hf.state_dict()
@@ -123,9 +170,13 @@ class GPT(nn.Module):
         sd_keys_hf = sd_hf.keys()
         # sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')]
         sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')]
-        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
-        # pytorch reverse the weight dimension for calculation performance 
-        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        transposed = [
+            'attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight',
+            'mlp.c_proj.weight'
+        ]
+        # pytorch reverse the weight dimension for calculation performance
+        assert len(sd_keys_hf) == len(
+            sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
         for k in sd_keys_hf:
             if any(k.endswith(w) for w in transposed):
                 assert sd_hf[k].shape[::-1] == sd[k].shape
@@ -138,6 +189,3 @@ class GPT(nn.Module):
 
         return model
 
-
-model = GPT.from_pretrained('gpt2')
-print("didn't crash yay!")
